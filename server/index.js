f9cf9c50892620f3aecbfb88456fd4da127c511f
aravinth7 <aravinth@shamlatech.net>
2026-03-09 16:47:35 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ const ALLOWED_ORIGINS = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:3000',
+  'http://localhost:8080',
   'https://safehaven-with-blockchain-1.onrender.com', // Frontend on Render
   process.env.FRONTEND_URL // Custom frontend URL from env
 ].filter(Boolean);
